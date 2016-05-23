5f2f3e4f925923d9936d1bd729696a380cf08b48
Juneko <ju_ails@hotmail.com>
2016-05-23 16:01:45 -0300

    Comunicação Visual session
    Layout e transição da sessão Comunicação Visual

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,16 +1,30 @@
 $(function() {
+  smoothScrool(800);
   workBelt();
 
 });
 
+function smoothScrool (duration){
+  $('a[href^="#"]').on('click', function(event){
+    var target=$($(this).attr('href'));
+
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, duration);
+    }
+  });
+}
+
 function workBelt() {
-
-  $('.thumb-unit').click(function() {
-    $('.work-belt').css('left','-100%');
+  $('.thumb-unit').click(function () {
+    $('.work-belt').css('left', '-100%');
+    $('.work-container').show();
   });
 
-  $('.work-return').click(function() {
-    $('.work-belt').css('left','0%');
+  $('.work-return').click(function () {
+    $('.work-belt').css('left', '0%');
+    $('.work-container').hide(800);
   });
-
 }
