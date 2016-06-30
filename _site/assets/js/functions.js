b7657083e57673cae8f3957370e64f79c9db4bed
Juneko <ju_ails@hotmail.com>
2016-06-30 14:21:53 -0300

    final tweaks

---
+++ _site/assets/js/functions.js
@@ -0,0 +1,49 @@
+$(function() {
+  smoothScrool(800);
+  workBelt();
+  workLoad();
+});
+
+function smoothScrool (duration){
+  $('a[href^="#"]').on('click', function(event){
+    var target=$($(this).attr('href'));
+
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, duration);
+    }
+  });
+}
+
+function workBelt() {
+  $('.thumb-unit').click(function () {
+    $('.work-belt').css('left', '-100%');
+    $('.work-container').show();
+  });
+
+  $('.work-return, .work-return-bottom').click(function () {
+    $('.work-belt').css('left', '0%');
+    $('.work-container').hide();
+  });
+}
+
+function workLoad() {
+
+  $.ajaxSetup({ cache: false });
+
+  $('.thumb-unit').click(function(){
+
+    var $this = $(this),
+        newTitle = $this.find('strong').text(),
+        newFolder = $this.data('folder'),
+
+        spinner = '<div class="loader">Loading...</div>',
+        newHTML = '/work/'+ newFolder +'.html';
+
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(newTitle);
+
+  });
+}
